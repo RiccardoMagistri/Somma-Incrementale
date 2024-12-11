1c8e23759ae706d58a41349508364f874a279556
RiccardoMagistri <riccardo.magistri@davincimilazzo.edu.it>
2024-12-11 16:42:54 +0100

    Delete tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-while (n != 0)
-                {
-                    somma = somma + n;
-                    scanf("%d", &n);
-                }
-                printf("La somma di tutti i numeri e': %d\n", somma);
