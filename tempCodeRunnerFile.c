c4d786c5e3a21b711b1920a4ccfe074293c6f0ba
RiccardoMagistri <riccardo.magistri@davincimilazzo.edu.it>
2024-12-11 16:42:23 +0100

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+while (n != 0)
+                {
+                    somma = somma + n;
+                    scanf("%d", &n);
+                }
+                printf("La somma di tutti i numeri e': %d\n", somma);
